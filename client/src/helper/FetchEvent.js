80a0abbaf84d5d636f5ac6f5dad388e3dd4cac9f
Ravi Chuahan <ravichauhan0407@gmail.com>
2023-01-05 19:56:08 +0530

    fourth commit

--- client/src/helper/FetchEvent.js
+++ client/src/helper/FetchEvent.js
@@ -2,20 +2,20 @@ const axios = require('axios').default;
 
 export const fetchUpcommingEvent=async ()=>
 {
-        const response =await axios.get(`${process.env.REACT_APP_URL}/api/get-uppcomming-Events`)
+        const response =await axios.get(`/api/get-uppcomming-Events`)
        
         return response.data;
 }
 
 export const fetchLiveEvent=async ()=>
 {
-        const response =await axios.get(`${process.env.REACT_APP_URL}/api/get-live-Events`)
+        const response =await axios.get(`/api/get-live-Events`)
         return response.data;
 }
 
 export const fetchPastEvent=async ()=>
 {
-        const response =await axios.get(`${process.env.REACT_APP_URL}/api/get-past-Events`)
+        const response =await axios.get(`/api/get-past-Events`)
         return response.data
 }
 
